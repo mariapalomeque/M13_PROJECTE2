c61f891d490bdac0a78585a6ab0e969378cf48b9
mariapalomeque <2023_maria.palomeque@iticbcn.cat>
2024-05-22 14:58:13 +0200

    Configuracion e instrucciones

--- remember/app/src/main/java/com/example/remember/RegistroPaciente.java
+++ remember/app/src/main/java/com/example/remember/RegistroPaciente.java
@@ -1,6 +1,10 @@
 package com.example.remember;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,14 @@ public class RegistroPaciente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registro_paciente);
+
+        Button btIniciarSesion = findViewById(R.id.iniciar_secion_paciente);
+        btIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegistroPaciente.this, presentacion.class);
+                startActivity(i);
+            }
+        });
     }
 }
